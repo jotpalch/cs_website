005fa525ba8524d0202588d1657dee806f5db424
jotpalch <jotp0910729001@gmail.com>
2022-02-16 19:53:05 +0800

    add top-bar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="logo-box">
+      <div class="name-box">
+        <a href="/">
+          <h1 class="site-name">國立陽明交通大學-資訊工程學系</h1>
+        </a>
+        <span class="site-tageline">高中生專區</span>
+      </div>
+    </div> 
+  </div>
 </template>
 
 <script>
@@ -15,6 +25,8 @@ export default {
 </script>
 
 <style>
+@import '~bootstrap/scss/bootstrap';  
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
