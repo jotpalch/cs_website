8429f7036c22907ba36c7a4ed72c9659b54e5e75
jotpalch <jotp076315217@gmail.com>
2022-03-16 10:50:22 +0800

    logo & title

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
     devServer: {
         disableHostCheck: true,
-    }
+    },
+    chainWebpack: config => {
+        config
+          .plugin('html')
+          .tap(args => {
+            args[0].title= '陽明交大資工系-高中生專區'
+            return args
+          })
+      }
 }
